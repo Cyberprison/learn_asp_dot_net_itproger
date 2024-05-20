54c6d575549cedda6c93fd0fba2a10986d0d4209
whoami <az.khabibullin2018@gmail.com>
2024-05-20 16:37:07 +0500

    create Data folder, inside folder interfaces, mocks and Modles create model Cat and Category create interfaces and mocks???

---
+++ Shop/Data/mocks/MockCars.cs
@@ -0,0 +1,51 @@
+﻿using Shop.Data.interfaces;
+using Shop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data.mocks
+{
+    public class MockCars : IAllCars
+    {
+
+        private readonly ICarsCategory _categoryCars = new MockCategory();
+
+        public IEnumerable<Car> Cars
+        {
+            get
+            {
+                return new List<Car>
+                {
+                    new Car {
+                        name = "tesla",
+                        shortDesc = "",
+                        longDesc = "",
+                        img = "",
+                        price = 45000,
+                        isFavourite = true,
+                        available = true,
+                        Category = _categoryCars.AllCategories.First()
+                    }
+                    new Car {
+                        name = "lada",
+                        shortDesc = "",
+                        longDesc = "",
+                        img = "",
+                        price = 5000,
+                        isFavourite = false,
+                        available = false,
+                        Category = _categoryCars.AllCategories.Last()
+                    }
+                };
+            }
+        }
+        public IEnumerable<Car> getFavCars { get; set; }
+
+        public Car getObjectCar(int carId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
